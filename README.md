4656e70b8de0f1c9084772e7987d6d20775b9131
wangyanan <wangyanan@youzan.com>
2018-03-26 10:33:07 +0800

    feat: 完成第一个可用版本

---
+++ README.md
@@ -0,0 +1,44 @@
+
+[![NPM](http://nodei.co/npm/params-verifier.png?downloads=true)](http://nodei.co/npm/params-verifier/)
+
+[![NPM version](https://img.shields.io/npm/v/params-verifier.svg)]()
+
+- params-verifier is a validator which can be used in controller of server or any other place.
+
+
+### install
+
+```shell
+    npm install params-verifier --save
+```
+
+
+### import
+- this package is developed by es6 syntax, so we recommend to load the package as follows:
+```javascript
+    import Validator from 'params-verifier';
+```
+or you can also load like this:
+```javascript
+    const Validator = require('params-verifier');
+```
+### Demo
+
+```javascript
+
+```
+
+
+### introduction
+
+##### test
+- start unit test
+
+```shell
+    npm test
+```
+
+- you can also test this node package in [es6 development environment](https://github.com/Yann-Wang/params-verifier-demo)
+
+### License
+- ISC
---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class HoverDelayTrigger extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ref = null;
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
+  }
+
+  handleMouseOver() {
+    const { delay } = this.props;
+    const ref = setTimeout(() => {
+      this.props.handleHoverTrigger();
+    }, delay);
+    this.ref = ref;
+  }
+
+  handleMouseOut() {
+    clearTimeout(this.ref);
+  }
+
+  empty() {}
+
+  render() {
+    return (
+      <div
+        className={this.props.className}
+        onHoverChange={this.props.handleHoverTrigger}
+        delay={this.props.delay}
+        onMouseOver={this.handleMouseOver}
+        onMouseOut={this.handleMouseOut}
+        onFocus={this.empty}
+        onBlur={this.empty}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+HoverDelayTrigger.propTypes = {
+  className: PropTypes.string,
+  handleHoverTrigger: PropTypes.func.isRequired,
+  delay: PropTypes.number.isRequired,
+  children: PropTypes.element
+};
+
+HoverDelayTrigger.defaultProps = {
+  className: '',
+  children: null
+};
